0d6a000c81f0cc1cc44b3bcc4d4b7251ee639d0f
unknown <jonasazula8901@gmail.com>
2023-03-15 10:19:14 -0500

    primer commit

---
+++ Pruebas/imprimir.py
@@ -0,0 +1,52 @@
+lupi = 12
+name= "lucia"
+
+print(lupi)
+print('la edad del usuario es:',lupi)
+
+e_civil = "soltera"
+
+print("la persona {} es {}" .format(e_civil,name))
+
+print("{1} es {0}" .format(e_civil,name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Pruebas/variable.py
@@ -0,0 +1,34 @@
+#comentarios hello
+#TODO : FALTA LA LOGICA DE PROGRAMACION
+
+nombre = 'Keliy'
+edad= 23
+
+description="""hola
+como estas"""
+
+print(description)
+# variables numericas
+year = 2022
+
+#type() => muestra el tipo de variable que es 
+print(type(year))
+print(type(edad))
+
+varibale = None
+print(type(varibale))
+
+mes, dia = "enero", 20
+
+print(mes, dia)
+
+# del > elimina la variable
+
+
+
+
+
+
+
+
+
